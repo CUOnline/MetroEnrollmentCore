6d058c4469bac879131bd1d81c8e15077d9e7662
Jacob Parker <jacob.2.parker@ucdenver.edu>
2018-11-09 13:23:00 -0700

    Initial Commit

---
+++ MetroEnrollment.Web/Models/CVSMetroEnrollmentEntry.cs
@@ -0,0 +1,26 @@
+﻿using SimpleCsvParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetroEnrollment.Web.Models
+{
+    public class CVSMetroEnrollmentEntry
+    {
+        [CsvProperty("first name")]
+        public string FirstName { get; set; }
+        [CsvProperty("last name")]
+        public string LastName { get; set; }
+        [CsvProperty("email")]
+        public string Email { get; set; }
+        [CsvProperty("id")]
+        public int Id { get; set; }
+        [CsvProperty("course code")]
+        public string CourseCode { get; set; }
+        [CsvProperty("course number")]
+        public int CourseNumber { get; set; }
+        [CsvProperty("section number")]
+        public string SectionNumber { get; set; }
+    }
+}
---
+++ MetroEnrollment.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MetroEnrollment.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
